6281f75908084c07e4d1bcd4cc9a8f5ce203aa30
Šimon <simon.potocnak03@gmail.com>
2022-12-07 13:06:39 +0100

    Created the initial code structure

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import HelloWorld from './HelloWorld.js';
+
+const greeting = new HelloWorld();
+console.log(greeting.greet());
+window.addEventListener('load', () => {
+  document.getElementById('content').innerText = greeting.greet();
+}
+);
